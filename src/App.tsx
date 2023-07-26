3d655dd8e2cbc41c7cbb749da3c6523aaf2bd413
Polina <polina-postylyakova@yandex.ru>
2023-07-27 01:23:25 +0300

    fixed menu, menu popup

--- src/App.tsx
+++ src/App.tsx
@@ -24,33 +24,28 @@ export const App = () => {
             An enthusiastic <span className={styles.contrastText}>developer</span>.
           </h1>
 
-          <div className={styles.bottomMenuEmptyContainer}>
-            <div className={styles.bottomMenuBlock}>
-              <ArrowDown className={styles.arrowDown} />
-              <div className={styles.bottomMenuButtons}>
-                <button><h2>Get in touch</h2></button>
-                <div className={styles.menuButtonArea}>
-                  {
-                    isMenuOpened &&
-                      <div>
-                        <ul>
-                          <li>About me</li>
-                          <li>Work</li>
-                          <li>Blog</li>
-                          <li>Contact</li>
-                        </ul>
-                      </div>
-                  }
-                  <button className={styles.menuButton} onClick={() => setMenuOpened((prevState) => !prevState)}>
-                    <h2>Menu</h2>
-                    {
-                      !isMenuOpened
-                          ? <BurgerMenu className={styles.burgerMenu} />
-                          : <CloseIcon className={styles.burgerMenu} />
-                    }
-                  </button>
-                </div>
-              </div>
+          <ArrowDown className={styles.arrowDown} />
+
+          <div className={styles.bottomMenuButtons}>
+            <button><h2>Get in touch</h2></button>
+            <div className={styles.menuButtonArea}>
+              {
+                  isMenuOpened &&
+                  <ul className={styles.menuOptions}>
+                    <li>About me</li>
+                    <li>Work</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                  </ul>
+              }
+              <button className={styles.menuButton} onClick={() => setMenuOpened((prevState) => !prevState)}>
+                <h2>Menu</h2>
+                {
+                  !isMenuOpened
+                      ? <BurgerMenu className={styles.burgerMenu} />
+                      : <CloseIcon className={styles.burgerMenu} />
+                }
+              </button>
             </div>
           </div>
 
